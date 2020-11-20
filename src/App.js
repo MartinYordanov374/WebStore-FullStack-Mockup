4a5002f3a52313e1ffb61bf88cae5bc4774e2f07
Marto330 <prophetyordanov@gmail.com>
2020-11-20 19:41:22 +0200

    Store is now persisted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Header'
+import Products from './Components/Products'
+import {store} from './Components/store'
+import {Provider} from 'react-redux'
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Navbar></Navbar>
+        <Products></Products>
+      </div>
+    </Provider>
   );
 }
 
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
-
-export default class Products extends Component {
+import {connect} from 'react-redux'
+class Products extends Component {
     render() {
+        let {products} = this.props
+        const productsList = [
+            {
+                name: 'test1',
+                image: 'image1',
+                price: 'testPrice',
+                description: 'lorem ipsum lol'
+            },
+            {
+                name: 'test1',
+                image: 'image1',
+                price: 'testPrice',
+                description: 'lorem ipsum lol'
+            }
+        ]
         return (
             <div>
-                
+                {productsList.map(product=><div>{product.name}</div>)}
             </div>
         )
     }
 }
+const mapStateToProps=(state={products:[{}]})=>{
+    return{
+        products: state.products
+    }
+}
+export default connect(mapStateToProps)(Products)
