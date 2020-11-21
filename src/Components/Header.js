3abc62ab11a56774bf275991a4f72e9b35da8496
Marto330 <prophetyordanov@gmail.com>
2020-11-21 10:03:06 +0200

    Changed the navbar styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,21 @@ import React from 'react'
 import {Navbar, Nav, NavbarBrand, NavItem, NavLink} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/Header.css'
+import {FaHeart, FaShoppingCart} from 'react-icons/fa'
 export default function Header() {
     return (
         <div>
             <Navbar variant='light'>
-                <NavbarBrand>TestBrand</NavbarBrand>
+                <NavbarBrand href='#'>Home</NavbarBrand>
+
+                <NavbarBrand className='navbarBrand'>Aurora</NavbarBrand>
                 <Nav className='ml-auto'>
                     <NavItem>
                         <NavLink>
-                            Home
+                            <FaHeart size={20}></FaHeart>
+                        </NavLink>
+                        <NavLink>
+                            <FaShoppingCart size={20}></FaShoppingCart>
                         </NavLink>
                     </NavItem>
                 </Nav>
