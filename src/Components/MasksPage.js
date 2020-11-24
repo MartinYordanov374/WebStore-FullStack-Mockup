e5200adcc255b4855ad033d01ac6438759980939
Marto330 <prophetyordanov@gmail.com>
2020-11-24 21:32:40 +0200

    The shopping cart is now done

--- src/Components/MasksPage.js
+++ src/Components/MasksPage.js
@@ -7,6 +7,7 @@ import {Button} from 'react-bootstrap'
 import MissingImage from './Images/noImage.png'
 import {store} from './store'
 import $ from 'jquery'
+import {v4} from 'uuid'
 export default function MasksPage() {
     const masksList=[
         {
@@ -14,23 +15,23 @@ export default function MasksPage() {
             image: MissingImage,
             price: 5.50,
             quantity: 1,
-            id: 0
+            id: v4()
         },
         {
             name: 'Anime Mask - Naruto - Sasuke',
             image: MissingImage,
             price: 3.40,
             quantity: 1,
-            id: 1
+            id: v4()
         },
     
     ]
     const addToWishlist=(product, id)=>{
-        store.dispatch({type: 'addToFavorites', name: product.name, image: product.image, price: product.price, cents: product.cents, id: product.id})
+        store.dispatch({type: 'addToFavorites', name: product.name, image: product.image, price: product.price, id: product.id, quantity: product.quantity})
         $('#'+id).css('color', 'purple')        
     }
     const addToCart=(product)=>{
-        store.dispatch({type: 'addToCart', name: product.name, image: product.image, price: product.price, cents: product.cents, id: product.id})
+        store.dispatch({type: 'addToCart', name: product.name, image: product.image, price: product.price, id: product.id, quantity: product.quantity})
     }
     return (
         
@@ -39,10 +40,10 @@ export default function MasksPage() {
             <div className='products'>
                 {masksList.map((mask, index)=>
                 <div className='hoodieWrapper'>
-                    <p className='hoodieName'><FaHeart onClick={()=>addToWishlist(mask)}id={index} className='addFaves' size={25}/><strong>{mask.name}</strong> </p>     
+                    <p className='hoodieName'><FaHeart onClick={()=>addToWishlist(mask)} className='addFaves' size={25}/><strong>{mask.name}</strong> </p>     
                     <img className='hoodieImage' src={mask.image}></img>
                     <div className='purchaseField'>
-                        <p><strong>{mask.price}.<span className='priceCents'>{mask.cents}</span> лв.</strong></p>
+                        <p><strong>{mask.price} лв.</strong></p>
                         <Button className='buyHoodieButton' variant='warning' onClick={()=>addToCart(mask)}><span className='buyIcon'><FaShoppingCart></FaShoppingCart></span> <strong>Купи</strong></Button>
                     </div> 
                 </div>)}
