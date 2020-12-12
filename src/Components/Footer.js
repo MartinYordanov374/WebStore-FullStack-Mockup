83cb848d04e5ed9354af0b848ab86494711de9d1
Marto330 <prophetyordanov@gmail.com>
2020-12-12 19:39:45 +0200

    fixed the website name

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className='row'>
                 <div className='footerAbout col-sm-6 col-lg-6 col-xs-6'>
                     <p className='footerText'>За нас</p>
-                    <p className='footerText'>Аврора е сайт за дрехи и аксесоари за всички възрасти. Ние вярваме, че забавните дрехи и аксесоари са за всички и това е нашата цел. При нас може да закупите забавни маски и худита.</p>
+                    <p className='footerText'><strong>Neon</strong> е сайт за дрехи и аксесоари за всички възрасти. Ние вярваме, че забавните дрехи и аксесоари са за всички и това е нашата цел. При нас може да закупите забавни маски и худита.</p>
                 </div>
                 <div className='footerLinks col-sm-6 col-lg-6 col-xs-6'>
                     <p className='footerText'>Връзки</p>
