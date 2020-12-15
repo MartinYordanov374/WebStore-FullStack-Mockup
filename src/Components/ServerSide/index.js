66f92aa3ddafe3d979217bc0a250c2db223fbd18
Marto330 <prophetyordanov@gmail.com>
2020-12-15 11:08:52 +0200

    You can now succesfully log in

--- src/Components/ServerSide/index.js
+++ src/Components/ServerSide/index.js
@@ -33,26 +33,25 @@ app.post('/register', (req, res)=>{
 app.post('/login', (req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
-    db.query('SELECT * FROM users WHERE username=? AND password=?', username, (err,res)=>{
-        if(err) 
+    db.query('SELECT * FROM users WHERE username=?;', username, (err,result)=>{
+        if(err)
         {
             res.send({err: err})
         }
-        if(res.length>0)
+        if(result.length>0)
         {
-            bcrypt.compare(password, res[0].password, (err, result)=>{
-                if(result)
-                {
-                    result.send(result)
-                }
-                else{
-                    result.send({message: 'Wrong user name-password combination'})
-                }
-            })
-           res.send({message: 'successfully logged in'})
+           bcrypt.compare(password, result[0].password, (err, response)=>{
+               if(response)
+               {
+                   res.send(result);
+               }
+               else{
+                   res.send({message: 'Wrong username/password combo'})
+               }
+           })
         }
         else{
-            res.send({message: 'User does not exist'})
+            res.send({message: 'User doesnt exist'})
         }
     })
 })
