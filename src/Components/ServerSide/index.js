76fff99c12d89f6090c7c65b588e8d9bb5ba1fd8
Marto330 <prophetyordanov@gmail.com>
2020-12-15 09:52:38 +0200

    Passwords are now hashed

--- src/Components/ServerSide/index.js
+++ src/Components/ServerSide/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
+const bcrypt = require('bcrypt')
 
 app = express()
 app.use(express.json())
@@ -16,9 +17,18 @@ db = mysql.createConnection({
 app.post('/register', (req, res)=>{
     const username = req.body.username;
     const password = req.body.password;
-    db.query('INSERT INTO users (username, password) VALUES (?,?)', [username, password], (err,res)=>{
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, (err,hash)=>
+    {
+        if(err)
+        {
+            console.log(err)
+        } 
+        db.query('INSERT INTO users (username, password) VALUES (?,?)', [username, hash], (err,res)=>{
         console.log(err)
     })
+    });
+   
 })
 app.post('/login', (req,res)=>{
     const username = req.body.username;
