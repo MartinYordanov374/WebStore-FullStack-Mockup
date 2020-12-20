5b8a0e67bc120fde2ec777eead3bda460b6d2a07
Marto330 <prophetyordanov@gmail.com>
2020-12-20 18:10:18 +0200

    Added alerts to the register form

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './css/login.css'
 import Navbar from './Header'
 import Footer from './Footer'
-import {FormControl, Button} from 'react-bootstrap'
+import {FormControl, Button, Alert} from 'react-bootstrap'
 import {FaRegUser, FaKey} from 'react-icons/fa'
 import Axios from 'axios'
 import $ from 'jquery'
@@ -11,19 +11,57 @@ export default function Register() {
     const [userReg, setUserReg] = useState('')
     const [passReg, setPassReg] = useState('')
 
+    useEffect(()=>{
+        hideAlerts()
+    },[])
     const register=()=>{
-        	Axios.post('http://localhost:3307/register',{
-                username: userReg,
-                password: passReg
-            }).then((response)=>{
-                console.log(response)
-            })
+            if($('.enterPassword').val().length>8)
+            {
+                Axios.post('http://localhost:3307/register',{
+                         username: userReg,
+                         password: passReg
+                     }).then((response)=>{
+                         console.log(response)
+                })
+                showAlertSuccess()
+            }
+            else
+            {
+                showAlertFail()
+            }
+    }
+
+    const hideAlerts=()=>{
+        $('.alert-success').fadeOut()
+        $('.alert-danger').fadeOut()
+
+    }
+    const hideAlertSuccess=()=>{
+        $('.alert-success').fadeOut()
+    }
+    const showAlertSuccess=()=>{
+        $('.alert-success').css('opacity', 1)
+        $('.alert-success').fadeIn()
+        hideAlertFail()
+
+    }
+    const hideAlertFail=()=>{
+        $('.alert-danger').fadeOut()
+    }
+    const showAlertFail=()=>{
+        $('.alert-danger').css('opacity', 1)
+        $('.alert-danger').fadeIn()
+        hideAlertSuccess()
+
     }
     return (
         <div className='loginFormWrapper'>
             <Navbar/>
             <div className='loginForm'>
                 <h2>Регистрация</h2>
+                <Alert className='alert-success' onClick={hideAlertSuccess}> <strong>Регистрацията е успешна!</strong></Alert>
+                <Alert className='alert-danger' onClick={hideAlertFail}> <strong> Въвели сте грешно име или парола !</strong></Alert>
+
                 <FormControl className='enterUsername' onChange={(e)=>setUserReg(e.target.value)} placeholder='Въведете никнейм'/>
                 <FormControl className='enterPassword'  onChange = {(e)=>setPassReg(e.target.value)} type='password' placeholder='Въведете парола'/>
                 <Button className='submitLoginForm' onClick ={register}><FaKey/> Завърши регистрация </Button>
