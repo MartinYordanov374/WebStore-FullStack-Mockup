e433e7a62a27e4560a03402ee727d7942ebe5db9
Marto330 <prophetyordanov@gmail.com>
2021-01-02 13:30:42 +0200

    Updated the navbar design; Changed the color schema of the navbar; Added a searchbar; The navbar now sticks to the top when scrolling;

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,29 +1,30 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {Navbar, Nav, NavbarBrand, NavItem, NavLink} from 'react-bootstrap'
+import {Navbar, Nav, NavbarBrand, NavItem, NavLink, FormControl, Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/Header.css'
-import {FaHeart, FaShoppingCart, FaUserCircle} from 'react-icons/fa'
+import {FaHeart, FaShoppingCart, FaUserCircle, FaSearch} from 'react-icons/fa'
 class Header extends Component {
     render() { 
         const {productsInWishList} = this.props;
         const {productsInCart} = this.props;
         let isLoggedIn = document.cookie
-        console.log('Header cookies:', isLoggedIn)
     return (
-        <div>
-            <Navbar variant='light'>
-                <NavbarBrand href='/Home' className='homeButton'><strong>Neon</strong></NavbarBrand>
+        <div className='navbarWrapper sticky-top'>
+            <Navbar className='sticky-top' variant='light'>
+                <NavbarBrand href='/Home'><span className='homeButton'><strong>Neon</strong></span></NavbarBrand>
                 <NavItem>
                     
-                    <NavLink className='nav-link' href='/Hoodies'>
+                    <NavLink className='HoodiesLink' href='/Hoodies'>
                         Худита
                     </NavLink>
-                    <NavLink href='/Masks'>
+                    <NavLink className='MasksLink' href='/Masks'>
                         Маски
                     </NavLink>
                 </NavItem>
-                <Nav className='ml-auto'>
+                <FormControl className='searchBar' placeholder='Потърсете продукт...'/>
+                <FaSearch className='searchButton'/>
+                <Nav className='Icons'>
                     <NavItem>    
                         { isLoggedIn.length > 3 
                         ?                    
