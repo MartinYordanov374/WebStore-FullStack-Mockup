9a0f6260532b710603f1336106c8aa8a03dc268f
Marto330 <prophetyordanov@gmail.com>
2021-01-12 20:33:39 +0200

    Added borders around the items in the wishlist

--- src/Components/Wishlist.js
+++ src/Components/Wishlist.js
@@ -16,33 +16,29 @@ class Wishlist extends Component {
         const removeFromWishList=(product)=>{
             store.dispatch({type:'removeFromWishList', name: product.name, image: product.image, price: product.price, cents: product.cents, id: product.id})
         }
+        console.log(productsInWishList.length<=0)
         return (
             <div>
                 <Navbar/>
-                <div className='wishlistWrapper'>
-                    {productsInWishList.length===0 ?
-                    <p>Вашият лист с любими е празен.</p> :
-                    ""}
-                    <div className='wishlistContainer'>
-                    {productsInWishList.map(product=>    
-                    
-                        <div className='productsWishListWrapper'>
-                            <p className='productName'> <strong>{product.name}</strong>
-                                <span>
-                                    <BsX className='removeProduct' size={25} onClick={()=>removeFromWishList(product)}/>
-                                </span>
-                            </p>
-                            <img className='productImage' src={product.image}/>
-                            <p className='productPrice'> <strong>{product.price} лв.</strong> </p>
-                            <Button className='addProductButton'><FaShoppingCart className='addProductIcon' size={25} onClick={()=>addToCart(product)}/> <strong>Добави в количката</strong></Button>
-                        </div>
+                <div className={productsInWishList.length<=0 ? '' : 'wishlistWrapper'}>
+                        {productsInWishList.length<=0 ?
                         
-                    )
-                        }
-                    </div>
+                        <p>Вашият лист с любими е празен.</p> 
+                        :
+                        <div className='wishlistContainer'>
+                            {productsInWishList.map((product)=>    
+                                <div className='productsWishListWrapper'>
+                                    <p className='productName'> <strong>{product.name}</strong>
+                                        <span>
+                                            <BsX className='removeProduct' size={25} onClick={()=>removeFromWishList(product)}/>
+                                        </span>
+                                    </p>
+                                    <img className='productImage' src={product.image}/>
+                                    <p className='productPrice'> <strong>{product.price} лв.</strong> </p>
+                                    <Button className='addProductButton'><FaShoppingCart className='addProductIcon' size={25} onClick={()=>addToCart(product)}/> <strong>Добави в количката</strong></Button>
+                                </div>)}
+                        </div>}
                 </div>
-
-            
             </div>
         )
     }
